bc2a34e05a0d4b5cf0911a238986734a1296897c
onaway <547896182@qq.com>
2021-06-10 19:08:14 +0800

    增加文章详情页

---
+++ src/templates/Post.vue
@@ -0,0 +1,39 @@
+<template>
+  <Layout>
+    <h1>{{ $page.post.title }}</h1>
+    <p>{{ $page.post.content }}</p>
+  </Layout>
+</template>
+
+<page-query>
+query ($id: ID!) {
+  post (id: $id) {
+    id
+    title
+    content
+  }
+}
+</page-query> 
+
+<script>
+export default {
+  // 通过返回函数的方式获取动态的标题
+  metaInfo () {
+    return {
+      title: this.$page.post.title
+    }
+  },
+  name: 'Post',
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style lang='less' scoped>
+</style>
