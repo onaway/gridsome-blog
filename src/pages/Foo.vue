563fb13573824f5656d95e846d052191ec466855
onaway <547896182@qq.com>
2021-06-10 13:03:29 +0800

    代码格式化

--- src/pages/Foo.vue
+++ src/pages/Foo.vue
@@ -1,23 +1,25 @@
 <template>
-    <div>
-        Foo Page
-    </div>
+  <div>
+    Foo Page
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Foo',
-    data () {
-        return {
-            
-        };
-    },
-    methods: {
-        
-    }
+  metaInfo: {
+    title: 'Foo'
+  },
+  name: 'Foo',
+  data() {
+    return {
+
+    };
+  },
+  methods: {
+
+  }
 }
 </script>
 
 <style lang='less' scoped>
-
 </style>
