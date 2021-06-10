38c7b00b942a1e0e07be6075dc0f44ab8b6e068a
onaway <547896182@qq.com>
2021-06-10 19:08:53 +0800

    配置集合

--- gridsome.server.js
+++ gridsome.server.js
@@ -5,14 +5,32 @@
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
-// gridsome 服务端的配置文件
+// gridsome 服务端的配置文件，通过 commonJs 导入模块
+
+const axios = require('axios');
 
 module.exports = function (api) {
-  api.loadSource(({ addCollection }) => {
+  api.loadSource(async ({ addCollection }) => {
     // Use the Data Store API here: https://gridsome.org/docs/data-store-api/
+    // 数据预取
+    const collection = addCollection('Post')
+
+    const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts')
+
+    for (const item of data) {
+      collection.addNode({
+        id: item.id,
+        title: item.title,
+        content: item.body
+      })
+    }
   })
 
   api.createPages(({ createPage }) => {
     // Use the Pages API here: https://gridsome.org/docs/pages-api/
+    // createPage({
+    //     path: '/my-page',
+    //     component: './src/templates/MyPage.vue'
+    // })
   })
 }
