37bc3a5c1df54ebe2a4706bfd18b9f6db782132e
onaway <547896182@qq.com>
2021-06-11 14:24:43 +0800

    首页数据渲染

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -19,63 +19,20 @@
       <div class="row gx-4 gx-lg-5 justify-content-center">
         <div class="col-md-10 col-lg-8 col-xl-7">
           <!-- Post preview-->
-          <div class="post-preview">
+          <div class="post-preview" v-for="post in $page.posts.edges" :key="post.node.id">
             <a href="post.html">
-              <h2 class="post-title">Man must explore, and this is exploration at its greatest</h2>
-              <h3 class="post-subtitle">Problems look mighty small from 150 miles up</h3>
+              <h2 class="post-title">{{ post.node.title }}</h2>
             </a>
             <p class="post-meta">
               Posted by
-              <a href="#!">Start Bootstrap</a>
-              on September 24, 2021
+              <a href="#!">{{ post.node.create_by.username }}</a>
+              {{ post.node.created_at |  date('MMM DD, YYYY') }}
             </p>
+
+            <!-- Divider-->
+            <hr class="my-4" />
           </div>
-          <!-- Divider-->
-          <hr class="my-4" />
-          <!-- Post preview-->
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">I believe every human has a finite number of heartbeats. I don't intend to waste any
-                of mine.</h2>
-            </a>
-            <p class="post-meta">
-              Posted by
-              <a href="#!">Start Bootstrap</a>
-              on September 18, 2021
-            </p>
-          </div>
-          <!-- Divider-->
-          <hr class="my-4" />
-          <!-- Post preview-->
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">Science has not yet mastered prophecy</h2>
-              <h3 class="post-subtitle">We predict too much for the next year and yet far too little for the next ten.
-              </h3>
-            </a>
-            <p class="post-meta">
-              Posted by
-              <a href="#!">Start Bootstrap</a>
-              on August 24, 2021
-            </p>
-          </div>
-          <!-- Divider-->
-          <hr class="my-4" />
-          <!-- Post preview-->
-          <div class="post-preview">
-            <a href="post.html">
-              <h2 class="post-title">Failure is not an option</h2>
-              <h3 class="post-subtitle">Many say exploration is part of our destiny, but it’s actually our duty to future
-                generations.</h3>
-            </a>
-            <p class="post-meta">
-              Posted by
-              <a href="#!">Start Bootstrap</a>
-              on July 8, 2021
-            </p>
-          </div>
-          <!-- Divider-->
-          <hr class="my-4" />
+          
           <!-- Pager-->
           <div class="d-flex justify-content-end mb-4"><a class="btn btn-primary text-uppercase" href="#!">Older Posts
               →</a></div>
@@ -84,6 +41,27 @@
     </div>
   </Layout>
 </template>
+
+<page-query>
+query {
+  posts: allStrapiPost {
+    edges {
+      node {
+        id
+        title
+        created_at
+        create_by {
+          username
+        }
+        tags {
+          id
+          title
+        }
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 export default {
