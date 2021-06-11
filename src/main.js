9effb31d07912451fc42543c90bb058733e33add
onaway <547896182@qq.com>
2021-06-12 00:19:37 +0800

    引入 @fortawesome/fontawesome-free 样式

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import dayjs from 'dayjs'
+import '@fortawesome/fontawesome-free/css/all.min.css'
 import './assets/css/index.css'
 import DefaultLayout from '~/layouts/Default.vue'
 
