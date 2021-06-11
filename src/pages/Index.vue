b7ccc5653d9be0ebbded5f8d4bf54b68edebcec2
onaway <547896182@qq.com>
2021-06-12 00:48:40 +0800

    顶部数据渲染

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,13 +1,13 @@
 <template>
   <Layout>
     <!-- Page Header-->
-    <header class="masthead" style="background-image: url('/img/home-bg.jpg')">
+    <header class="masthead" :style="{backgroundImage: `url(http://localhost:1337${general.cover.url})`}">
       <div class="container position-relative px-4 px-lg-5">
         <div class="row gx-4 gx-lg-5 justify-content-center">
           <div class="col-md-10 col-lg-8 col-xl-7">
             <div class="site-heading">
-              <h1>Clean Blog</h1>
-              <span class="subheading">A Blog Theme by Start Bootstrap</span>
+              <h1>{{ general.title }}</h1>
+              <span class="subheading">{{ general.subtitle }}</span>
             </div>
           </div>
         </div>
@@ -69,6 +69,19 @@ query ($page: Int) {
       }
     }
   }
+
+  general: allStrapiGeneral {
+    edges {
+      node {
+        id
+        title
+        subtitle
+        cover {
+          url
+        }
+      }
+    }
+  }
 }
 </page-query>
 
@@ -84,6 +97,11 @@ export default {
   components: {
     Pager,
   },
+  computed: {
+    general() {
+      return this.$page.general.edges[0].node
+    }
+  }
 };
 </script>
 
