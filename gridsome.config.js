3a4fbacf06cb450e0c69052d811849ff9daf0097
onaway <547896182@qq.com>
2021-06-11 23:49:49 +0800

    添加 tag 标签页

--- gridsome.config.js
+++ gridsome.config.js
@@ -10,13 +10,15 @@ module.exports = {
   siteName: 'blog', // 页面名称，即页面 tab 标签上的 title
   siteDescription: 'blog',  // meta description
   pathPrefix: '',   // 部署目录 若在 app 目录部署，则为 ’/app‘
+  port: 3080,
+  // configureWebpack: {}, // 自定义 webpack 配置
   plugins: [
     {
       use: '@gridsome/source-strapi',
       options: {
         apiURL: 'http://localhost:1337',
         queryLimit: 1000, // Defaults to 100
-        contentTypes: ['post'],
+        contentTypes: ['post', 'tag'],
         // singleTypes: ['impressum'],
         // Possibility to login with a Strapi user,
         // when content types are not publicly available (optional).
@@ -27,13 +29,17 @@ module.exports = {
       }
     }
   ],
-  port: 3080,
-//   configureWebpack: {}, // 自定义 webpack 配置
   templates: {
-    Post: [
+    StrapiPost: [
       {
         path: '/post/:id',
         component: './src/templates/Post.vue'
+      }
+    ],
+    StrapiTag: [
+      {
+        path: '/tag/:id',
+        component: './src/templates/Tag.vue'
       }
     ]
   }
