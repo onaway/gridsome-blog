2f1d626855f2e51ef1556d8e6e4e1f8040f6c2c7
onaway <547896182@qq.com>
2021-06-11 23:50:43 +0800

    a 标签改为 g-link

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -3,7 +3,7 @@
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-light" id="mainNav">
       <div class="container px-4 px-lg-5">
-        <a class="navbar-brand" href="index.html">Start Bootstrap</a>
+        <g-link class="navbar-brand" to="/">Start Bootstrap</g-link>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
           aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           Menu
@@ -11,10 +11,10 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarResponsive">
           <ul class="navbar-nav ms-auto py-4 py-lg-0">
-            <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="index.html">Home</a></li>
-            <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="about.html">About</a></li>
-            <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="post.html">Sample Post</a></li>
-            <li class="nav-item"><a class="nav-link px-lg-3 py-3 py-lg-4" href="contact.html">Contact</a></li>
+            <li class="nav-item"><g-link class="nav-link px-lg-3 py-3 py-lg-4" to="/">Home</g-link></li>
+            <li class="nav-item"><g-link class="nav-link px-lg-3 py-3 py-lg-4" to="/about">About</g-link></li>
+            <li class="nav-item"><g-link class="nav-link px-lg-3 py-3 py-lg-4" to="/posts">Sample Post</g-link></li>
+            <li class="nav-item"><g-link class="nav-link px-lg-3 py-3 py-lg-4" to="/contact">Contact</g-link></li>
           </ul>
         </div>
       </div>
