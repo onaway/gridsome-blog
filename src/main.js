47dc389ae43a279b8cbe1019dd0869875e65b43f
onaway <547896182@qq.com>
2021-06-12 12:44:40 +0800

    全局混入变量 GRIDSOME_API_URL

--- src/main.js
+++ src/main.js
@@ -7,6 +7,13 @@ import DefaultLayout from '~/layouts/Default.vue'
 
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
+  Vue.mixin({
+    data( {
+      return {
+        GRIDSOME_API_URL: process.env.GRIDSOME_API_URL
+      }
+    })
+  })
   Vue.component('Layout', DefaultLayout)
 
   Vue.filter('date', (value, format = 'YYYY-MM-DD HH:mm:ss') => {
