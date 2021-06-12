18054b54051cb4709058970e453020108eeb7303
onaway <547896182@qq.com>
2021-06-12 12:58:22 +0800

    全局混入变量 GRIDSOME_API_URL

--- src/main.js
+++ src/main.js
@@ -8,11 +8,11 @@ import DefaultLayout from '~/layouts/Default.vue'
 export default function (Vue, { router, head, isClient }) {
   // Set default layout as a global component
   Vue.mixin({
-    data( {
+    data() {
       return {
         GRIDSOME_API_URL: process.env.GRIDSOME_API_URL
       }
-    })
+    }
   })
   Vue.component('Layout', DefaultLayout)
 
